927c911e62459fd2aabfbf822a9f18ec740e0546
nguyen chi thanh <n.c.thanh0801@gmail.com>
2016-12-05 14:33:43 +0700

    login-logout

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_many :comments, :rates, :favorites, :orders, :suggested_products
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { sessions: 'users/sessions' }
+  # devise_scope :user do
+  #   get 'sign_in', to: 'devise/sessions#new'
+  # end
   root "static_pages#home"
 
   get "/:page", to: "static_pages#show"
